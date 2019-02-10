9f4c63e6a1f0737a2c012bb97c567ab6519c06c6
sirocco1 <shino2163@gmail.com>
2019-02-10 17:21:20 +0900

    db修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_070637) do
+ActiveRecord::Schema.define(version: 2019_02_10_075531) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -45,8 +45,6 @@ ActiveRecord::Schema.define(version: 2019_02_10_070637) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "last_name", default: "f", null: false
-    t.string "first_name", default: "f", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
