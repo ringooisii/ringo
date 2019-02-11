b37ccfbce7c11efc172a35cab44582371b6dc922
能勢大輔 <nosedaisuke@nosedaisukenoMacBook-Air.local>
2019-02-11 17:55:06 +0900

    gemlock変更

--- db/migrate/20190210063452_create_shippings.rb
+++ db/migrate/20190210063452_create_shippings.rb
@@ -1,7 +1,10 @@
 class CreateShippings < ActiveRecord::Migration[5.2]
   def change
     create_table :shippings do |t|
-
+    	t.integer :user_id
+    	t.integer :shipping_address, null: false
+    	t.string  :shipping_postcode, null: false
+    	t.string  :shipping_name, null: false
       t.timestamps
     end
   end
